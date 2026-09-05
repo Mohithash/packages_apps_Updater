40936adbb7936c365c685f5ad215b5e90aee6eee
Mohithash <shahjespuri@gmail.com>
2026-09-05 14:50:29 +0000

    Updater: Read the bestrom device and version props
    The build emits ro.bestrom.device and org.bestrom.version now, so
    matching against the old ro.voltage.device and org.voltage.version
    never finds the device or a newer build.
    
    Signed-off-by: Mohithash <shahjespuri@gmail.com>

--- app/src/main/java/com/bestrom/updater/misc/Constants.java
+++ app/src/main/java/com/bestrom/updater/misc/Constants.java
@@ -40,8 +40,8 @@ public final class Constants {
 
     public static final String PROP_AB_DEVICE = "ro.build.ab_update";
     public static final String PROP_BUILD_DATE = "ro.build.date.utc";
-    public static final String PROP_BUILD_VERSION = "org.voltage.version";
-    public static final String PROP_DEVICE = "ro.voltage.device";
+    public static final String PROP_BUILD_VERSION = "org.bestrom.version";
+    public static final String PROP_DEVICE = "ro.bestrom.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
     public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "lineage.updater.allow_downgrading";
 
